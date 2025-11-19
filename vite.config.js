f42f7f9866ed461942218708365075e47e561f11
Jemanuel514 <jemanuel0123@hotmail.com>
2025-11-18 23:58:07 -0500

    perf(SecondLife): minor changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.js',
                 'resources/css/style.css',
                 'resources/css/catalog_style.css',
                 'resources/css/catalog_detail_style.css',
