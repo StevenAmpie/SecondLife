c2d1448a899f373ae4693653f1b57c2a6b2424e8
Jemanuel514 <jemanuel0123@hotmail.com>
2025-11-18 19:43:36 -0500

    feat(SecondLife): add styles to edit article view

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,21 @@
 
 use App\Http\Controllers\CatalogoController;
 use App\Http\Controllers\PublicacionController;
+use App\Http\Controllers\ArticuloController;
 use Illuminate\Support\Facades\Route;
 
 //Put your routes view here
 
 // You can code a route group like this
-
 /*Route::controller(EntityNameController::class)->group(function () {
     Route::get('/name', [Entity::class, 'method_1']);
     Route::post('/name/{parameter}', [EntityController::class, 'method_2']);
 });*/
 
-Route::get('/publish', [PublicacionController::class, 'create']);
 #catalog
 Route::controller(CatalogoController::class)->group(function () {
     Route::get('/catalog', [CatalogoController::class, 'index'])->name('catalogo.index');
     Route::get('/catalog/{id}', [CatalogoController::class, 'show'])->name('catalogo.show');
 });
-
-
+Route::get('/edit_article', [ArticuloController::class, 'edit']);
+Route::get('/publish', [PublicacionController::class, 'create']);
