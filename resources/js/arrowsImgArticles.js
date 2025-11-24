630e7f1ee766a396be67056e704455ce18577a9b
StevenAmpie <ampiesteven24@gmail.com>
2025-11-24 00:01:12 -0500

    feat(SecondLife): Adjusted the js logic to match the changes in the styles and layout

--- resources/js/arrowsImgArticles.js
+++ resources/js/arrowsImgArticles.js
@@ -1,34 +1,51 @@
 
 function activeArrowBtn(media_query){
 
-    const article_container = document.querySelectorAll('.images-articles-layout');
+
+
     if (media_query.matches) {
+        let article_container = document.querySelectorAll('.images-articles-layout');
 
         article_container.forEach(function (container){
 
+            let images_container = container.getElementsByTagName("img");
+            let imagen1 = [images_container[0].src, images_container[0].alt];
+            let imagen2 = [images_container[1].src, images_container[1].alt];
             container.innerHTML = `
 
                     <button class="arrow-btn" style="visibility: hidden;">
                         <img src="/images/arrow-btn-left.png" alt="arrow-btn-left" width="100%" height="50%">
                     </button>
-                    <img src="/images/portada-publicacion3.jpg" alt="pantalones" width="150" height="150">
+                    <div class="images-container">
+                        <img src="${images_container[0].src}" alt="${images_container[0].alt}" width="150" height="150">
+                        <img src="${images_container[1].src}" alt="${images_container[1].alt}"
+                                style="display: none;" width="150" height="150">
+                    </div>
                     <button class="arrow-btn">
                                 <img src="/images/arrow-btn-right.png" alt="arrow-btn-right" width="100%" height="50%">
                     </button>
             `
-            changeVisibility(container)
+            changeVisibility(container, imagen1, imagen2)
         })
     } else {
+
+        let article_container = document.querySelectorAll('.images-articles-layout');
+
+
         article_container.forEach(function (container){
 
+            let card = container.querySelector('.images-container');
+            let images_card = card.getElementsByTagName('img');
+            let imagen1 = [images_card[0].src, images_card[0].alt];
+            let imagen2 = [images_card[1].src, images_card[1].alt];
             container.innerHTML = `
 
                      <div class="article_image">
-                          <img src="/images/portada-publicacion3.jpg" alt="pantalones1" width="180" height="180">
+                        <img src="${imagen1[0]}" alt="${imagen1[1]}" width="180" height="180">
                      </div>
 
                      <div class="article_image">
-                        <img src="/images/portada-publicacion2.jpg" alt="pantalones2" width="180" height="180">
+                        <img src="${imagen2[0]}" alt="${imagen2[1]}" width="180" height="180">
                     </div>
 
             `;
@@ -38,22 +55,32 @@ function activeArrowBtn(media_query){
 function changeVisibility(container){
 
     const arrows = container.getElementsByTagName('button');
-    const img = container.getElementsByTagName('img');
+    const card = container.querySelector('.images-container');
+    const img = card.getElementsByTagName('img')
+
     arrows[0].addEventListener('click', function(){
 
         arrows[0].style.visibility = 'hidden';
         arrows[1].style.visibility = 'visible';
-        img[1].src = "/images/portada-publicacion3.jpg";
+
+
+        img[0].style.display = 'block';
+        img[1].style.display = 'none';
 
     })
     arrows[1].addEventListener('click', function (){
 
+
         arrows[0].style.visibility = 'visible';
-        arrows[1].style.visibility = 'hidden';
-        img[1].src = "/images/portada-publicacion2.jpg";
+        arrows[1].style.visibility = 'hidden'
+
+        img[0].style.display = 'none';
+        img[1].style.display = 'block';
+
+
+
 
     })
-
 
 }
 
--- resources/js/filtersMobile.js
+++ resources/js/filtersMobile.js
@@ -1,13 +1,12 @@
 
 const main_container = document.getElementById('main');
 const first_section_in_main = document.getElementById("filters_block");
-const filter_btn = document.getElementById("filters-button_id");
 const background_catalogo = document.getElementById('catalogo_id')
 const filters_component = document.getElementById('filters_block');
 
-const filters_mobile_container = document.createElement('section');
+const filters_mobile_container = document.createElement('form');
 
-const filter_title_mobile = document.createElement('h1');
+const filter_title_mobile = document.createElement('h2');
 filter_title_mobile.innerText = 'Filtros';
 filter_title_mobile.id = 'filter-title-mobile-id';
 
@@ -27,20 +26,60 @@ filters_mobile_container.appendChild(filter_title_mobile);
 filters_mobile_container.appendChild(checkBoxes_filters_mobile);
 filters_mobile_container.appendChild(filter_btn_mobile);
 
+const root = document.getElementById('filters_block');
+
+filters_mobile_container.action = root.action;
+filters_mobile_container.method = 'GET';
 filters_mobile_container.className = 'filters_mobile';
 filters_mobile_container.id = 'filters-mobile-container-id';
-filter_btn.addEventListener('click', function (event){
 
-    if(document.getElementById('filters-mobile-container-id')){
-        filters_mobile_container.remove();
+const media_query = window.matchMedia("(max-width: 480px)");
+const filters_btn = document.createElement('button');
 
-    }else{
-        main_container.insertBefore(filters_mobile_container, first_section_in_main)
+function handleFilters(e) {
 
+    const search_container = document.querySelector('.search-container');
+
+    if (e.matches) {
+
+        if (!document.getElementById('filters-button-id')) {
+
+
+            filters_btn.innerHTML = `
+                <img id="filter-img" src="http://localhost:8000/images/filters-icon.png" alt="Botón de filtros">
+            `;
+            filters_btn.type = 'button';
+            filters_btn.className = 'filters-button';
+            filters_btn.id = 'filters-button-id';
+
+            if (search_container) {
+                search_container.insertBefore(filters_btn, search_container.firstChild);
+            }
+        }
+
+    } else {
+        const btn = document.getElementById('filters-button-id');
+        if (btn) btn.remove();
     }
+}
+
+handleFilters(media_query);
+
+
+media_query.addEventListener("change", handleFilters);
+
+filters_btn.addEventListener('click', function (){
+
+if(document.getElementById('filters-mobile-container-id')){
+    filters_mobile_container.remove();
+
+}else{
+    main_container.insertBefore(filters_mobile_container, first_section_in_main)
+
+}
 
 });
-background_catalogo.addEventListener('click', function (event){
+    background_catalogo.addEventListener('click', function (){
 
     filters_mobile_container.remove();
 });
