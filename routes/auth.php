3d0a4a5125061af6cd402287d489198658ca9b90
Steven Ampie <ampiesteven24@gmail.com>
2025-11-30 11:56:06 -0500

    refactor(SecondLife): refactored all routes, and group by entity and access

--- routes/auth.php
+++ routes/auth.php
@@ -1,35 +1,44 @@
 <?php
 
+use App\Http\Controllers\ArticuloController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\PagoController;
+use App\Http\Controllers\PublicacionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
-
-
-    Route::controller(UsuarioController::class)->group(function () {
-
-        Route::get('register', 'create')
-            ->name('register');
-
-        Route::post('register', 'store')
-            ->name('register.store');
-
-    });
-
-    Route::controller(AuthenticatedSessionController::class)->group(function () {
-
-        Route::get('login', 'create')
-            ->name('login');
-
-        Route::post('login', 'store')
-            ->name('login.store');
-
-    });
-
-});
 
 Route::middleware('auth')->group(function () {
+
+    Route::controller(PublicacionController::class)->group(function () {
+
+        Route::get('/my-publications', 'index')
+            ->name('publicaciones.index');
+
+        Route::get('/publish', 'create')
+            ->name('publish.create');
+
+        Route::put('/update-state/{id}', 'updateState')
+            ->name('update-state');
+
+        Route::get('/edit-publication', 'show')
+            ->name('publicaciones.show');
+
+        Route::delete('delete/{id_post}', 'destroy')
+            ->name('publicaciones.destroy');
+
+    });
+
+    Route::controller(ArticuloController::class)->group(function () {
+
+        Route::get('/edit/{id_post}/{id_article}', 'edit');
+
+    });
+
+    Route::controller(PagoController::class)->group(function () {
+
+        Route::get('/payment', 'create');
+
+    });
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\CatalogoController;
 use App\Http\Controllers\PublicacionController;
 use App\Http\Controllers\PagoController;
 use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 require __DIR__.'/auth.php';
 
-//Put your routes view here
-
-// You can code a route group like this
-/*Route::controller(EntityNameController::class)->group(function () {
-    Route::get('/name', [Entity::class, 'method_1']);
-    Route::post('/name/{parameter}', [EntityController::class, 'method_2']);
-});*/
-
 Route::redirect('/', '/catalog');
-#catalog
+
 Route::controller(CatalogoController::class)->group(function () {
     Route::get('/catalog', 'index')->name('catalogo.index');
     Route::get('/catalog/publication/{id}', 'show')->name('catalogo.show');
@@ -25,20 +19,28 @@ Route::controller(CatalogoController::class)->group(function () {
     Route::get('/catalog/filters', 'filters')->name('catalogo.filters');
 });
 
-Route::get('/payment', [PagoController::class, 'create'])->middleware('auth');
-Route::get('/edit/{id_post}/{id_article}', [ArticuloController::class, 'edit']);
-Route::get('/publish', [PublicacionController::class, 'create'])->name('publish.create');
+Route::middleware('guest')->group(function () {
 
-#my_publications
 
-// Ruta principal del módulo "Mis publicaciones"
-Route::get('/publicaciones', [PublicacionController::class, 'index'])
-    ->name('publicaciones.index');
+    Route::controller(UsuarioController::class)->group(function () {
 
-Route::get('/my-publications', function () {
-    return redirect()->route('publicaciones.index');
-})->name('publicaciones.mias');
+        Route::get('register', 'create')
+            ->name('register');
 
-// Vista estática de "Edit publication".
-// Más adelante esta ruta se cambiara
-Route::get('/edit-publication', [PublicacionController::class, 'show']);
+        Route::post('register', 'store')
+            ->name('register.store');
+
+    });
+
+    Route::controller(AuthenticatedSessionController::class)->group(function () {
+
+        Route::get('login', 'create')
+            ->name('login');
+        Route::post('login', 'store')
+            ->name('login.store');
+
+    });
+
+});
+
+
