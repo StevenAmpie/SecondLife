39d8f3a8425eb746c84734965e6237ecf6185e5e
Steven Ampie <ampiesteven24@gmail.com>
2025-11-30 11:58:07 -0500

    feat(SecondLife): improve the view style, added the component and backend logic

--- resources/views/my_publications.blade.php
+++ resources/views/my_publications.blade.php
@@ -9,118 +9,34 @@
     {{-- Contenido principal --}}
     <x-slot name="edit_article_main">
         <h1>MIS PUBLICACIONES</h1>
+        @if($status == 200)
+            <section class="my-publications-container">
 
-        <section class="my-publications-container">
+                   @foreach($publications as $publication)
 
-            {{-- PUBLICACIÓN 1 - Disponible --}}
-            <article class="publication-card">
-                <div class="publication-image-wrapper">
-                    <img
-                        src="{{ asset('images/portada-publicacion1.jpg') }}"
-                        alt="Jeans Levi L"
-                        class="publication-image"
-                    >
-                </div>
+                        @if($publication->estado == 'Disponible')
 
-                <div class="publication-info">
-                    <p class="publication-title">Jeans Levi L</p>
-                    <p class="publication-price">20.00$</p>
-                    <p class="publication-date">09/10/2025</p>
-                    <p class="publication-status disponible">Disponible</p>
-                </div>
+                            <x-my-publications.disponibleCard :publication="$publication">
+                            </x-my-publications.disponibleCard>
 
-                <div class="publication-actions">
-                    <button type="button" class="small-button">
-                        Ocultar publicación
-                    </button>
-                    <button type="button" class="small-button">
-                        Editar publicación
-                    </button>
-                    <button type="button" class="btn-delete">
-                        Eliminar publicación
-                    </button>
-                </div>
-            </article>
+                        @elseif($publication->estado == 'Oculta')
 
-            {{-- PUBLICACIÓN 2 - Oculta --}}
-            <article class="publication-card">
-                <div class="publication-image-wrapper">
-                    <img
-                        src="{{ asset('images/portada-publicacion2.jpg') }}"
-                        alt="Jeans Levi M"
-                        class="publication-image"
-                    >
-                </div>
+                        <x-my-publications.ocultaCard :publication="$publication">
+                        </x-my-publications.ocultaCard>
 
-                <div class="publication-info">
-                    <p class="publication-title">Jeans Levi M</p>
-                    <p class="publication-price">18.00$</p>
-                    <p class="publication-date">05/10/2025</p>
-                    <p class="publication-status oculta">Oculta</p>
-                </div>
+                        @elseif($publication->estado == 'Vendida')
 
-                <div class="publication-actions">
-                    <button type="button" class="small-button">
-                        Mostrar publicación
-                    </button>
-                    <button type="button" class="small-button">
-                        Editar publicación
-                    </button>
-                    <button type="button" class="btn-delete">
-                        Eliminar publicación
-                    </button>
-                </div>
-            </article>
+                            <x-my-publications.vendidaCard :publication="$publication">
+                            </x-my-publications.vendidaCard>
 
-            {{-- PUBLICACIÓN 3 - Disponible --}}
-            <article class="publication-card">
-                <div class="publication-image-wrapper">
-                    <img
-                        src="{{ asset('images/portada-publicacion3.jpg') }}"
-                        alt="Jeans Negro S"
-                        class="publication-image"
-                    >
-                </div>
+                        @endif
 
-                <div class="publication-info">
-                    <p class="publication-title">Jeans Negro S</p>
-                    <p class="publication-price">22.00$</p>
-                    <p class="publication-date">01/10/2025</p>
-                    <p class="publication-status disponible">Disponible</p>
-                </div>
+                   @endforeach
 
-                <div class="publication-actions">
-                    <button type="button" class="small-button">
-                        Ocultar publicación
-                    </button>
-                    <button type="button" class="small-button">
-                        Editar publicación
-                    </button>
-                    <button type="button" class="btn-delete">
-                        Eliminar publicación
-                    </button>
-                </div>
-            </article>
-
-            {{-- PUBLICACIÓN 4 - Vendida (sin botones) --}}
-            <article class="publication-card">
-                <div class="publication-image-wrapper">
-                    <img
-                        src="{{ asset('images/portada-publicacion2.jpg') }}"
-                        alt="Jeans Levi L Vendidos"
-                        class="publication-image"
-                    >
-                </div>
-
-                <div class="publication-info">
-                    <p class="publication-title">Jeans Levi L</p>
-                    <p class="publication-price">20.00$</p>
-                    <p class="publication-date">09/10/2025</p>
-                    <p class="publication-status vendida">Vendida</p>
-                </div>
-            </article>
-
-        </section>
+            </section>
+      @else
+        <div class="not-found-my-publications"><h2>{{$detail}}</h2></div>
+      @endif
     </x-slot>
 
 </x-general.layout>
