ffaae29fac696232aec485c7be1f0546a7e97def
Steven Ampie <ampiesteven24@gmail.com>
2025-11-30 11:55:20 -0500

    feat(SecondLife): added get, put and delete to publication entity

--- app/Http/Controllers/PublicacionController.php
+++ app/Http/Controllers/PublicacionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Publicacion;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class PublicacionController extends Controller
@@ -9,7 +11,18 @@ class PublicacionController extends Controller
     public function index() // Display a listing of the resource.
     {
         // Retornar la vista de mis publicaciones
-        return view('my_publications');
+        $user_id = auth()->user()->id;
+        $publications = Publicacion::select(['id', 'titulo', 'fecha', 'precio', 'estado', 'portada'])
+            ->where('id_usuario', $user_id)
+            ->get();
+
+        if ($publications->isEmpty()) {
+
+            return view('my_publications', ['detail' => 'No tienes publicaciones', 'status'=>404]);
+
+        }
+
+        return view('my_publications', ['publications' => $publications, 'status'=>200]);
     }
 
     public function create() // Show the form for creating a new resource.
@@ -40,8 +53,51 @@ class PublicacionController extends Controller
         //
     }
 
+    public function updateState(Request $request, string $id){
+
+        $user_id = auth()->user()->id;
+
+        $id_validate = Publicacion::select('id')
+            ->where('id_usuario', $user_id)
+            ->where('id', $id)
+            ->first();
+
+        if (!$id_validate) {
+
+            abort(403);
+
+        }
+
+        Publicacion::where('id', $id)->update([
+
+            'estado' => $request->estado
+
+        ]);
+
+        return redirect()->route('publicaciones.index');
+
+
+
+
+    }
+
     public function destroy(string $id) // Remove the specified resource from storage.
     {
-        //
+        $user_id = auth()->user()->id;
+
+        $id_validate = Publicacion::select('id')
+            ->where('id_usuario', $user_id)
+            ->where('id', $id)
+            ->first();
+
+        if (!$id_validate) {
+
+            abort(403);
+
+        }
+
+        Publicacion::where('id', $id)->delete();
+
+        return redirect()->route('publicaciones.index');
     }
 }
