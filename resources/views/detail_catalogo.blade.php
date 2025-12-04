a3d62ac3fddc6f4f20b81d03a69b30dd6fbaa2d3
Steven Ampie <ampiesteven24@gmail.com>
2025-12-03 22:49:41 -0500

    feat(SecondLife): added condition if a user can or not see the payment button

--- resources/views/detail_catalogo.blade.php
+++ resources/views/detail_catalogo.blade.php
@@ -15,14 +15,20 @@
                     <p>Por: {{$user_full_name->nombre." ".$user_full_name->apellido}}</p>
                     <p>Descripción: 3 Jeans en buen estado menos de 1 año de uso</p>
                 </article>
-                @if(auth()->user()->id !== $publication->id_usuario)
+                @if(auth()->check() && auth()->user()->id !== $publication->id_usuario)
                     <div>
-
                             <button onclick="window.location.href='{{route('pago.create', $publication->id)}}';">
                                 ${{$publication->precio}}
                             </button>
                     </div>
                 @endif
+                @guest
+                    <div>
+                        <button onclick="window.location.href='{{route('pago.create', $publication->id)}}';">
+                            ${{$publication->precio}}
+                        </button>
+                    </div>
+                @endguest
             </section>
             <section class="articulos">
 
